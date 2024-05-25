23c3462cd2e3301a5d46e41aa61d796d7c64bf5a
Tamaki <83083937+tamaki380@users.noreply.github.com>
2024-05-26 02:57:59 +0900

    通信機能は完成

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
+# server.py
 from flask import Flask, request, jsonify
+from pydub import AudioSegment
+import io
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Welcome to the Home Page!'
+@app.route('/process', methods=['POST'])
+def process():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+    file = request.files['file']
+    if file:
+        result = process_audio(file)
+        str_result = ', '.join(map(str, result))
+        return jsonify({'result': str_result})
+    else:
+        return jsonify({'error': 'Invalid file format'})
 
-@app.route('/hello/<name>')
-def hello(name):
-    return f'Hello, {name}!'
-
-@app.route('/data', methods=['POST'])
-def process_data():
-    data = request.get_json()
-    return jsonify({'received': data})
+def process_audio(file):
+    audio = AudioSegment.from_mp3(io.BytesIO(file.read()))
+    #24000Hzにダウンサンプリング
+    processed_data = [0,3,0,4,-7,10,5,6,8,11]
+    return processed_data
 
 if __name__ == '__main__':
     app.run(debug=True)
