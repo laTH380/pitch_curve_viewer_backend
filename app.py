9b4ac686d7e399eb855c29219864dd0e9be94ed6
Tamaki <83083937+tamaki380@users.noreply.github.com>
2024-05-29 03:31:05 +0900

    tyotto

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ from flask import Flask, request, jsonify
 from pydub import AudioSegment
 from pydub.playback import play
 import numpy as np
-from scipy.fft import fft
 import io
 import librosa
 from flask_cors import CORS
-import pprint
 
 app = Flask(__name__)
 CORS(app)
@@ -19,12 +17,10 @@ def process():
         return jsonify({'error': 'No file part'})
     file = request.files['file']
     if file:
-        result, times, length, maybe_result = process_audio(file)
+        result, times, length = process_audio(file)
         str_result = ', '.join(map(str, result))
         str_times = ', '.join(map(str, times))
-        str_maybe_result = ', '.join(map(str, maybe_result))
-        print('success', flush=True)
-        return jsonify({'result': str_result,'times': str_times,'length': str(length),'f0_full': str_maybe_result})
+        return jsonify({'result': str_result,'times': str_times,'length': str(length)})
     else:
         return jsonify({'error': 'Invalid file format'})
 
@@ -42,10 +38,8 @@ def process_audio(file):
     del downsampled_audio
     y, sr = librosa.load(wav_data)
     f0, voiced_flag, voiced_probs = librosa.pyin(y, fmin=librosa.note_to_hz('C2'), fmax=librosa.note_to_hz('C7'), sr = target_rate, frame_length = 2400, hop_length = 2400//4)
-    f00, voiced_flag, voiced_probs = librosa.pyin(y, fmin=librosa.note_to_hz('C2'), fmax=librosa.note_to_hz('C7'), sr = target_rate, frame_length = 2400, hop_length = 2400//4, fill_na=None)#hop_lengthの影響で設定したフレーム数の情報を得られない
-    print(f00,flush=True)
     times = librosa.times_like(f0, sr = target_rate, hop_length = 2400//4)
-    return f0, times, ad_length, f00
+    return f0, times, ad_length
 
 # 最も顕著なピッチを抽出する
 def get_pitch(pitches, magnitudes):
